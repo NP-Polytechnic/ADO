8316f8b63b30c021496bde6bc0d0c24095d6bc5b
George Ng <george_ng@np.edu.sg>
2023-12-16 06:41:37 +0000

    Added fresh_orders.sql, added fresh_orders as a source in src_pricesmart.yml, added fresh_orders as a table in dbt_project.yml, changed raw_orders.sql into an incremental model to append data into raw_orders table

--- models/raw/incremental_orders.sql
+++
@@ -1,7 +0,0 @@
--- models/incremental_orders.sql
-
-{{ config(materialized='incremental', unique_key='ORDERID') }}
-
-SELECT *
-FROM FRESH_ORDERS
-WHERE CAST(ORDERID AS BIGINT) > (SELECT MAX(CAST(ORDERID AS BIGINT)) FROM ORDERS)
